a1fa2bbd68f55172e36377e0f61f1815a3151b32
engfabiodesalvi <engfabiodesalvi@gmail.com>
2025-11-24 17:33:58 -0300

    feat: Navigation has been implemented via clicking on menu items and using a DOMContentLoaded listener.

--- praticaIII/js/app.js
+++ praticaIII/js/app.js
@@ -43,21 +43,63 @@ function showHiddenMenu() {
     menu.classList.add("active");
     // Set bar display style to none
     barsHamburguer.forEach((barHamburguer) => {
-        barHamburguer.style.display = "none";
+      barHamburguer.style.display = "none";
     });
     // Set close display style to block
-    closeHamburguer.style.display = "block";    
+    closeHamburguer.style.display = "block";
   } else {
     // Turn menu on
     menu.classList.remove("active");
     // Set bars display style to block
     barsHamburguer.forEach((barHamburguer) => {
-        barHamburguer.style.display = "block";
+      barHamburguer.style.display = "block";
     });
     // Set close display style to none
     closeHamburguer.style.display = "none";
   }
 }
+
+// Menu button click event listener
+hamburguer.addEventListener("click", toggleMenu);
+
+// Listen for an event after loading DOM content.
+document.addEventListener("DOMContentLoaded", (event) => {
+  menu.addEventListener("click", function (e) {
+    console.log(`${e.target} - ${e.target.nodeName}`);
+    // Checj if the clicked element (e.target) is a link
+    if (e.target && e.target.nodeName === "A") {
+      // Default action would not been taken
+      // This is used to process data before calling an anchoring target
+      e.preventDefault();
+
+      // Get de action from the data attribute
+      const action = e.target.getAttribute("data-action");
+
+      // Perform different actions based on the clicked item
+      switch (action) {
+        case "inicio":
+          console.log("Inicio link clicked! Load inicio  content.");
+          // To navigate for target
+          window.location.href = e.target;
+          break;
+        case "projetos":
+          console.log("Projetos link ckicked! Load about content.");
+          // To navigate for target
+          window.location.href = e.target;
+          break;
+        case "cadastro":
+          console.log("Cadastro link clicked! Load contact content.");
+          // To navigate for target
+          window.location.href = e.target;
+          break;
+
+        default:
+          console.log(`${action} link clicked! Load ${action} content.`);
+          break;
+      }
+    }
+  });
+});
 
 // Hello message!
 console.log("Hi! This is the PraticaIII Project!");
@@ -70,6 +112,3 @@ console.log(
   hamburguer.getAttribute("aria-expanded"),
   hamburguer.getAttribute("aria-expanded") === "true"
 );
-
-// Menu button click event listener
-hamburguer.addEventListener("click", toggleMenu);
